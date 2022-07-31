a040911b2a95d8ab862066b5decca23bdca0873a
Omer Erdem Aydemir <aydemirerdemomer@gmail.com>
2022-07-31 15:52:20 +0300

    firebase setup with cli and added initialize

--- lib/product/initialize/project_initialize.dart
+++ lib/product/initialize/project_initialize.dart
@@ -1,6 +1,8 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:salus/product/initialize/language_initialize.dart';
+import 'package:firebase_core/firebase_core.dart';
+import '../../firebase_options.dart';
 
 abstract class IProjectInitialize {
   IProjectInitialize(this.languageInitialize);
@@ -15,6 +17,7 @@ class ProjectInitialize implements IProjectInitialize {
   @override
   Future<void> setup() async {
     WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
     await EasyLocalization.ensureInitialized();
     EasyLocalization.logger.enableLevels = [];
   }
