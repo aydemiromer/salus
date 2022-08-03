ddfe3796f2b5daaf0cc1b34e8bf295b928c5677e
Omer Erdem Aydemir <aydemirerdemomer@gmail.com>
2022-08-04 00:20:01 +0300

    some packages added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:salus/feature/authentication/view/auth_view.dart';
 import 'package:salus/product/initialize/project_initialize.dart';
 import 'package:salus/product/utils/theme/app_theme.dart';
@@ -7,10 +8,13 @@ import 'package:salus/product/utils/theme/app_theme.dart';
 Future<void> main() async {
   final IProjectInitialize appInitialize = ProjectInitialize();
   await appInitialize.setup();
-  runApp(EasyLocalization(
-      path: appInitialize.languageInitialize.translationsPath,
-      supportedLocales: appInitialize.languageInitialize.supportedLocales,
-      child: const MyApp()));
+  runApp(MultiProvider(
+    providers: appInitialize.productProvider.provider,
+    child: EasyLocalization(
+        path: appInitialize.languageInitialize.translationsPath,
+        supportedLocales: appInitialize.languageInitialize.supportedLocales,
+        child: const MyApp()),
+  ));
 }
 
 class MyApp extends StatelessWidget {
