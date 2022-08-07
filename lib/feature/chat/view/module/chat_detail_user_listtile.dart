af8cfcd6ade1782b496bfbd1bae592cd7f99ec4b
Omer Erdem Aydemir <aydemirerdemomer@gmail.com>
2022-08-07 19:50:26 +0300

    bug fixed

--- lib/feature/chat/view/module/chat_detail_user_listtile.dart
+++ lib/feature/chat/view/module/chat_detail_user_listtile.dart
@@ -23,52 +23,49 @@ class _DetailListTile extends StatelessWidget {
           ],
         ),
         subtitle: Row(
-          children: [
-            _circleAvatarStatus(context),
-            context.emptySizedWidthBoxLow,
-            ProductText(user.status.toString())
-          ],
+          children: [_circleAvatarStatus(context), context.emptySizedWidthBoxLow, ProductText(user.status.toString())],
         ));
   }
 
   CircleAvatar _circleAvatarStatus(BuildContext context) {
     return CircleAvatar(
-            radius: 5,
-            backgroundColor: user.status.toString() == LocaleKeys.status_online.tr()
-                ? context.colorScheme.surface
-                : context.colorScheme.primary,
-          );
+      radius: 5,
+      backgroundColor: user.status.toString() == LocaleKeys.status_online.tr()
+          ? context.colorScheme.surface
+          : context.colorScheme.primary,
+    );
   }
 
   CircleAvatar _circleAvatar(BuildContext context) {
     return CircleAvatar(
-          radius: 30,
-          backgroundColor: context.colorScheme.onTertiary,
-          child: Align(
-            alignment: Alignment.center,
-            child: ProductText.semiBoldNormal(
-              '${user.name?[0]} ${user.surname?[0]} ',
-              context: context,
-            ),
-          ));
+        radius: 30,
+        backgroundColor: context.colorScheme.onTertiary,
+        child: Align(
+          alignment: Alignment.center,
+          child: ProductText.semiBoldNormal(
+            '${user.name?[0]} ${user.surname?[0]} ',
+            context: context,
+          ),
+        ));
   }
 }
 
-SizedBox _assignmentStatusCard(BuildContext context, UserModel user, ChatState state) {
-  return SizedBox(
-    child: InkWell(
-      onTap: (){},
-      child: Card(
-        color: state.colorCondition(context, user.assignment.toString()),
-        child: Padding(
-          padding: const PagePadding.allLow(),
-          child: ProductText(
-            user.assignment.toString(),
-          ).color(context.colorScheme.onSurface),
+Visibility _assignmentStatusCard(BuildContext context, UserModel user, ChatState state) {
+  return Visibility(
+    visible: user.assignment.toString() == "" ? false : true,
+    child: SizedBox(
+      child: InkWell(
+        onTap: () {},
+        child: Card(
+          color: state.colorCondition(context, user.assignment.toString()),
+          child: Padding(
+            padding: const PagePadding.allLow(),
+            child: ProductText(
+              user.assignment.toString(),
+            ).color(context.colorScheme.onSurface),
+          ),
         ),
       ),
     ),
   );
 }
-
-
